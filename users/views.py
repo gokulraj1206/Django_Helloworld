1d1fa2f75a34b82c5f29491871f3ea5855512590
gokul <gokulraj1206@gmail.com>
2020-03-12 20:07:09 +0530

    Fixed error While Registering

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 def register(request):
 
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST,request.FILES,instance=request.user)
+        form = UserRegisterForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
             # username= form.cleaned_data.get('username')
